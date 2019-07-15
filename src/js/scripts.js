751a1c8f520378a10db86bf755539bc31164c249
mariannematias <mariannec.matias@gmail.com>
2019-07-15 15:01:50 +0800

    fix scrolling on mobile

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,20 @@
 $(document).ready(function() {
 
     // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html, body').animate({
-                scrollTop: (target.offset().top + 10)
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function(e) {
+        e.preventDefault();
+        if ($(window).width() > 768) {
+            $('html, body').animate({
+                scrollTop: ($(this.hash).offset().top + 10)
                 }, 1200, "easeInOutExpo");
                 return false;
-            }
+        } else {
+            $('html, body').animate({
+                scrollTop: ($(this.hash).offset().top - 200)
+                }, 1200, "easeInOutExpo");
+                return false;
         }
+        
     });
 
     // Closes responsive menu when a scroll trigger link is clicked
